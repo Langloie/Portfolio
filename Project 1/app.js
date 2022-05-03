5d4c6cf1b96e83bf970a74c960673caff1006222
Langloie <Langloiea@gmail.com>
2022-05-03 14:58:16 -0400

    Added RPG Battle project

---
+++ Project 1/app.js
@@ -0,0 +1,59 @@
+//indicate if its players turn again
+//hide player and enemy turn divs respectively
+
+//prevent action while not attacking
+
+//hero attack reduces enemy health
+//enemy attack reduces hero health
+
+
+//enemy attacks player
+
+//fire magic attack 
+    //an attack that doesnt miss and does a lot of damage
+    
+
+//potion increase health
+    
+
+//run - lose game and ask to start over
+
+
+//if all enemies health === 0, alert you won the game
+//ask to start over
+
+//if player health === 0, you lose
+//ask to start over
+
+
+//separate function to start over and call in other state functions
+//location.reload()?
+
+
+//hero attacks are charged up and an onclick event determines the power of the attack
+
+//random spawn enemies
+//toggle class list 
+
+//if class === goblin, stats
+//hitpoints: 20
+//damage:5
+
+//if class === skeleton, stats
+//hitpoints: 10
+//damage: 10
+
+//if class === blob, stats
+//hitpoints: 5
+//damage: 5
+
+//select who to attack
+//if id === 1, attackenemy()
+//if id === 2, attackenemy()
+//if id === 3, attackenemy()
+
+//attackenemy(){
+//get enemy id
+//reduce hitpoints if > 0
+//toggle dead if hitpoints = 0
+
